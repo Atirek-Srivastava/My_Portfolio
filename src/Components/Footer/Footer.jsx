b61c91a492fadeabfb858e53cd8b8430f565bd77
Atirek-Srivastava <atirek2109@gmail.com>
2024-05-06 16:55:42 +0530

    added social media icon in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,10 @@ import './Footer.css'
 import logo from '../../assets/logo.png'
 import user_icon from '../../assets/user_icon.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import instagram from '../../assets/instagram_icon.png'
+import linkedin from '../../assets/linkedin.png'
+import twitter from '../../assets/twitter.avif'
+
 const Footer = () => {
   return (
     <>
@@ -14,7 +18,7 @@ const Footer = () => {
                 </div>
                 <div className="footer-top-right">
                     <div className="footer-email-input">
-                        <img src={user_icon} alt="" />
+                        {/* <img src={user_icon} alt="" /> */}
                         {/* <input type="email" placeholder='Enter your email' /> */}
                     </div>
                     {/* <div className="footer-subscribe">Subscribe</div>  */}
@@ -25,10 +29,16 @@ const Footer = () => {
                 <p className="footer-bottom-left">
                 © Atirek Srivastava 2024. All Rights Reserved.
                 </p>
-                <div className="footer-bottom-right">
-                    <p>Term of services</p>
-                    <p>Privecy Policy</p>
-                </div>
+                <div className="footer-bottom-right"><div className="footer-bottom-right-instagram">
+                    <a href='https://www.instagram.com/__atirek_/'> <img src={instagram} alt="" /></a>
+                    </div>
+                    <div className="footer-bottom-right-twitter"> 
+                    <a href='https://twitter.com/__atirek_'> <img src={twitter} alt="" /></a>
+                    </div>
+                    <div className="footer-bottom-right-linkedin">
+                    <a href='https://www.linkedin.com/in/atirek-srivastava-586151217/'> <img src={linkedin} alt="" /></a>
+                    </div></div>
+                
             </div>
         </div>
     </>
